da1011045b20e9c0c24215073dd8a20b5b856bc8
TheKoren <korenzol.007@gmail.com>
2023-06-29 17:28:30 +0200

    Created sidebar for multiple boards

--- prodify/src/Components/TaskList.js
+++ prodify/src/Components/TaskList.js
@@ -4,8 +4,10 @@ import TaskPopup from "./TaskPopup";
 import "./TaskList.css";
 
 function TaskList() {
-  const [tasks, setTasks] = useState([]);
+  const [boards, setBoards] = useState([]);
+  const [activeBoardIndex, setActiveBoardIndex] = useState(null);
   const [isPopupOpen, setIsPopupOpen] = useState(false);
+  const [newBoardName, setNewBoardName] = useState("");
 
   const openPopup = () => {
     setIsPopupOpen(true);
@@ -16,43 +18,127 @@ function TaskList() {
   };
 
   const addTask = (newTask) => {
-    setTasks((prevTasks) => [...prevTasks, newTask]);
-  };
-
-  const removeTask = (index) => {
-    setTasks((prevTasks) => prevTasks.filter((_, i) => i !== index));
-  };
-
-  const updateTask = (task, editedTask) => {
-    const updatedTasks = tasks.map((t) => {
-      if (t === task) {
-        return { ...t, ...editedTask };
-      }
-      return t;
+    setBoards((prevBoards) => {
+      const updatedBoards = [...prevBoards];
+      updatedBoards[activeBoardIndex].tasks.push(newTask);
+      return updatedBoards;
     });
-  
-    setTasks(updatedTasks);
+  };
+
+  const removeTask = (taskIndex) => {
+    setBoards((prevBoards) => {
+      const updatedBoards = [...prevBoards];
+      updatedBoards[activeBoardIndex].tasks.splice(taskIndex, 1);
+      return updatedBoards;
+    });
+  };
+
+  const updateTask = (taskIndex, editedTask) => {
+    setBoards((prevBoards) => {
+      const updatedBoards = [...prevBoards];
+      updatedBoards[activeBoardIndex].tasks[taskIndex] = {
+        ...updatedBoards[activeBoardIndex].tasks[taskIndex],
+        ...editedTask,
+      };
+      return updatedBoards;
+    });
+  };
+
+  const createBoard = () => {
+    if (newBoardName.trim() !== "") {
+      const newBoard = {
+        name: newBoardName,
+        tasks: [],
+      };
+      setBoards([...boards, newBoard]);
+      setActiveBoardIndex(boards.length);
+      setNewBoardName("");
+    }
+  };
+
+  const deleteBoard = () => {
+    if (activeBoardIndex !== null) {
+      setBoards((prevBoards) => {
+        const updatedBoards = [...prevBoards];
+        updatedBoards.splice(activeBoardIndex, 1);
+        return updatedBoards;
+      });
+      setActiveBoardIndex(null);
+    }
+  };
+
+  const editBoardName = (newName) => {
+    if (activeBoardIndex !== null) {
+      setBoards((prevBoards) => {
+        const updatedBoards = [...prevBoards];
+        updatedBoards[activeBoardIndex].name = newName;
+        return updatedBoards;
+      });
+    }
   };
 
   return (
-    <>
-      <button className="add-task-button" onClick={openPopup}>
-          Add Task
-        </button>
-
-      {isPopupOpen && <TaskPopup onClose={closePopup} onSave={addTask} />}
-
-      <ul className="task-list">
-        {tasks.map((task, index) => (
-          <TaskItem
-            key={index}
-            task={task}
-            onRemove={() => removeTask(index)}
-            onUpdate={updateTask}
+    <div className="task-list-container">
+      <div className="side-menu">
+        <h2 className="menu-heading">Boards</h2>
+        <ul className="board-list">
+          {boards.map((board, index) => (
+            <li
+              key={index}
+              className={`board-item ${index === activeBoardIndex ? "active" : ""}`}
+              onClick={() => setActiveBoardIndex(index)}
+            >
+              {board.name}
+            </li>
+          ))}
+        </ul>
+        <div className="board-actions">
+        <input
+            type="text"
+            className="new-board-name"
+            placeholder="Enter board name"
+            value={newBoardName}
+            onChange={(e) => setNewBoardName(e.target.value)}
           />
-        ))}
-      </ul>
-    </>
+          <button className="add-board-button" onClick={createBoard}>
+            Add Board
+          </button>
+          <button className="delete-board-button" onClick={deleteBoard}>
+            Delete Board
+          </button>
+          <input
+            type="text"
+            className="edit-board-name"
+            placeholder="Edit Board Name"
+            value={activeBoardIndex !== null ? boards[activeBoardIndex].name : ""}
+            onChange={(e) => editBoardName(e.target.value)}
+          />
+        </div>
+      </div>
+
+      <div className="task-content">
+        {activeBoardIndex !== null && (
+          <>
+            <button className="add-task-button" onClick={openPopup}>
+              Add Task
+            </button>
+
+            {isPopupOpen && <TaskPopup onClose={closePopup} onSave={addTask} />}
+
+            <ul className="task-list">
+              {boards[activeBoardIndex].tasks.map((task, index) => (
+                <TaskItem
+                  key={index}
+                  task={task}
+                  onRemove={() => removeTask(index)}
+                  onUpdate={(editedTask) => updateTask(index, editedTask)}
+                />
+              ))}
+            </ul>
+          </>
+        )}
+      </div>
+    </div>
   );
 }
 
